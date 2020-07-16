ae45bd391cdc803fca9df25ae18dfc79ca629ad6
anuranBarman <anuranbarman2k15@rediffmail.com>
2020-07-16 10:28:36 +0530

    Initial Commit

---
+++ Friendly Money/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Friendly Money
+//
+//  Created by Anuran Barman on 16/07/20.
+//  Copyright © 2020 Anuran Barman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
